21f583be5bbecd32d160a01ef95d45b5b116794b
IlyaMalyukov <ilya.maliukov@yandex.ru>
2021-04-02 11:15:26 +0300

    modal pay-online finish

---
+++ js/modal.js
@@ -0,0 +1,84 @@
+// Modal First
+
+modalBtnFirst.addEventListener('click', () => {
+  modalWindowFirst.style.display = 'block';
+});
+
+modalCloseFirst.addEventListener('click', () => {
+  modalWindowFirst.style.display = 'none';
+});
+
+modalOuterFirst.addEventListener('click', () => {
+  modalWindowFirst.style.display = 'none';
+});
+
+
+// ModalSecond
+
+modalBtnSecond.addEventListener('click', () => {
+  modalWindowSecond.style.display = 'block';
+});
+
+modalCloseSecond.addEventListener('click', () => {
+  modalWindowSecond.style.display = 'none';
+});
+
+modalOuterSecond.addEventListener('click', () => {
+  modalWindowSecond.style.display = 'none';
+});
+
+// ModalThird
+
+modalBtnThird.addEventListener('click', () => {
+  modalWindowThird.style.display = 'block';
+});
+
+modalCloseThird.addEventListener('click', () => {
+  modalWindowThird.style.display = 'none';
+});
+
+modalOuterThird.addEventListener('click', () => {
+  modalWindowThird.style.display = 'none';
+});
+
+// ModalFourth
+
+modalBtnFourth.addEventListener('click', () => {
+  modalWindowFourth.style.display = 'block';
+});
+
+modalCloseFourth.addEventListener('click', () => {
+  modalWindowFourth.style.display = 'none';
+});
+
+modalOuterFourth.addEventListener('click', () => {
+  modalWindowFourth.style.display = 'none';
+});
+
+// ModalFifth
+
+modalBtnFifth.addEventListener('click', () => {
+  modalWindowFifth.style.display = 'block';
+});
+
+modalCloseFifth.addEventListener('click', () => {
+  modalWindowFifth.style.display = 'none';
+});
+
+modalOuterFifth.addEventListener('click', () => {
+  modalWindowFifth.style.display = 'none';
+});
+
+// ModalSixth
+
+modalBtnSixth.addEventListener('click', () => {
+  modalWindowSixth.style.display = 'block';
+});
+
+modalCloseSixth.addEventListener('click', () => {
+  modalWindowSixth.style.display = 'none';
+});
+
+modalOuterSixth.addEventListener('click', () => {
+  modalWindowSixth.style.display = 'none';
+});
